53baf80466d82f3abc4db5ebe98c48bf6783cd72
Christopher Allen <cpcallen+git@google.com>
2017-06-29 12:00:51 +0100

    Also recognise node.js syntax errors

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -288,14 +288,20 @@
 
 (add-hook 'compilation-mode-hook
 	  (function (lambda ()
-		      ;; Add NodeJS error format
+		      ;; Add NodeJS error formats for stack traces & SyntaxError
 		      (add-to-list 'compilation-error-regexp-alist-alist
 				   '(node "^[  ]+at \\(?:[^\(\n]+ \(\\)?\\([a-zA-Z\.0-9_/-]+\\):\\([0-9]+\\):\\([0-9]+\\)\)?$"
 					  1 ;; file
 					  2 ;; line
 					  3 ;; column
 					  ))
-		      (add-to-list 'compilation-error-regexp-alist 'node))))
+		      (add-to-list 'compilation-error-regexp-alist-alist
+				   '(node-syn "^\\(/[^:]+\\):\\([0-9]+\\)\)?$"
+					  1 ;; file
+					  2 ;; line
+					  ))
+		      (add-to-list 'compilation-error-regexp-alist 'node)
+		      (add-to-list 'compilation-error-regexp-alist 'node-syn))))
 
 ;;; Font Lock Mode
 
